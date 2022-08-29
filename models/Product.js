9c8f6c10242b4c5323bed7e83552f105a1a18b6d
Jovy Ira <ira_bonching@yahoo.com>
2022-08-29 16:20:11 -0400

    set up fields and rules for Product model

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,32 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-  },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+        },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      },
+    price: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },    
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    }
+  },       
   {
     sequelize,
     timestamps: false,
