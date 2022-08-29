9533ed4b7c79feb70dfe7c92345f93f0eb923e51
Jovy Ira <ira_bonching@yahoo.com>
2022-08-29 16:59:52 -0400

    added function on category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,35 +3,33 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories   
-
-    Category.findAll({}, (err, categories) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send({
-        message: 'There was an error getting  the category listening  to your API. P'
-      })
-    }
-  })
-        });
-
+router.get('/', async (req, res) => {
+  // find all categories  
+  try {
+    const categories = await Category.findAll({
+      //Add Product as a second model to JOIN with
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+})
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value  
-    
   // be sure to include its associated Products
   try {
-    const libraryCardData = await LibraryCard.findByPk(req.params.id, {
-      include: [{ model: Reader }],
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
     });
 
-    if (!libraryCardData) {
+    if (!category) {
       res.status(404).json({ message: 'No library card found with that id!' });
       return;
     }
 
-    res.status(200).json(libraryCardData);
+    res.status(200).json(categories);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -41,48 +39,43 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new category
   try {
-    // Since the model will create a unique UUID value by default,    
-    //we just need to provide the `id` of the Reader that will own this card
-    const locationData = await LibraryCard.create({
-      reader_id: req.body.reader_id,
-    });
-    res.status(200).json(locationData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+    const data = await Category.create(req.body);
+      res.status(201).json(data);
+      } 
+      catch (err) { 
+        res.status(400).json(err); 
+      }
+});  
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
   try {
-    const userData = await User.update(req.body, {
+    const data = await Category.create(req.body,  {
       where: {
         id: req.params.id,
       },
     });
     if (!userData[0]) {
-      res.status(404).json({ message: 'No user with this id!' });
+      res.status(404).json({ message: 'No category with this id!' });
       return;
     }
     res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
   }
-
-
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
   try {
-    const libraryCardData = await LibraryCard.destroy({
+    const data = await Category.destroy({
       where: {
         id: req.params.id,
       },
     });
 
-    if (!libraryCardData) {
-      res.status(404).json({ message: 'No library card found with that id!' });
+    if (!data) {
+      res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
 
